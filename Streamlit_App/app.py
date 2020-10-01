84f5f1300c9d1544b1723bd6765a90ec1d85fb98
lucy-allen <66030273+lucy-allen@users.noreply.github.com>
2020-10-01 13:28:49 -0400

    Update app.py

--- Streamlit_App/app.py
+++ Streamlit_App/app.py
@@ -1,18 +1,16 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
-Created on Fri Sep  4 15:16:47 2020
-
-@author: lucyallen
+Uses Streamlit to create a web application to share the results
+in a user friendly way
 """
 
-
+# Importing the Necessary Packages and Modules
 import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
 from Function import find_resort, limit_region, limit_price, limit_distance, name_to_index, limit_beginner
 
+# Reading in the Data Frame used in this app
 resorts_df = pd.read_csv('Resorts_Clean.csv')
 resorts_df.drop('Unnamed: 0', axis=1, inplace=True)
 
@@ -21,11 +19,9 @@ st.image('Jan2016_small.jpg', width=700)
 st.markdown("---")
 st.subheader("Share your favorite Ski Resort and I will recommend you a new one, that I know you'll like!")
 
-
 no_fave = st.selectbox('Do You Have a Favorite Resort?', ['Yes', 'No'])
 'You selected:', no_fave
-
-  
+ 
 region = st.selectbox('Do you have a region of preference', ['ALL', 'Western US', 'Eastern US', 'Midwest US'])
 'You selected: ', region
 
@@ -47,11 +43,9 @@ if location == True:
     longditude = st.number_input('Please Enter your Longditude')
     latitude = st.number_input('Please Enter your Latitude')
     max_dist = st.number_input('Enter the maximum distance you would like to go (in mi)')
-    
-    
+        
 num_recs = st.selectbox('How many recommendations would you like to see?', range(1,11))
 'You want to see: ', str(num_recs), ' recommendations'
-
 
 resorts_df = limit_price(resorts_df, price_max)
 
@@ -64,9 +58,7 @@ if region != 'ALL':
 
 coords = resorts_df[['Latitude', 'Longditude']]
 coords.rename(columns={'Latitude':'lat', 'Longditude':'lon'}, inplace=True) 
-    
-    
-    
+   
 if no_fave == 'Yes':
     if st.button('Recommend Me Some Resorts'):
         try:
@@ -115,15 +107,9 @@ else:
                 index_vals.append(name_to_index(rec, resorts_df))
             for idx, rec in enumerate(recommendations):
                 st.write(str(idx+1) + '. ', rec)            
-            st.map(coords.iloc[index_vals])
-        
+            st.map(coords.iloc[index_vals])      
 
     st.write('Happy Skiing!!')
 
 st.image('Whitefish.JPG', width=700)
 st.write('Data Source: skiresort.info')
-
-
-
-
-
